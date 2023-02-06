0f603e7362447b1ef2364b71eb68d55dcef26d80
RLVictor <vityad888@yandex.ru>
2023-02-06 23:24:30 +0300

    06.02.23_mobile

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,47 @@ const swiper2 = new Swiper(".swiper:nth-of-type(2)", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+$(document).ready(function () {
+  $(".promo__button").on("click", function () {
+    $(".overlay, .modal").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, .modal, .feed-form").fadeOut("slow");
+    // setTimeout(() => window.location.reload(), 1000);
+  });
+
+  // Отправка на почту
+  $("form").submit(function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $(".modal, .feed-form").fadeOut("slow");
+      $(".modal__feed").fadeIn("slow");
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".mini__menu"),
+    hamburger = document.querySelector(".promo__header-hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("promo__header-hamburger-active");
+    menu.classList.toggle("mini__menu-active");
+  });
+
+  menuList.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("promo__header-hamburger-active");
+      menu.classList.toggle("mini__menu-active");
+    });
+  });
+});
