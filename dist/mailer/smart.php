491f67492b0ddd02ea5993e3aecaeb2ae5a9363f
RLVictor <vityad888@yandex.ru>
2023-02-07 23:29:07 +0300

    final

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -9,27 +9,17 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'vityad888@yandex.ru';                 // Наш логин
-$mail->Password = 'tlewvlnqwzyjhwdc';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
- 
-$mail->setFrom('vityad888@yandex.ru', 'tzezotje');   // От кого письмо 
-$mail->addAddress('vityad888@yandex.ru');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isSMTP();                                      
+$mail->Host = 'smtp.yandex.ru';  
+$mail->SMTPAuth = true;                               
+                        
+$mail->SMTPSecure = 'ssl';                            
+$mail->Port = 465;                                    
+    
 
-$mail->Subject = 'Сообщение с вашего сайта The Red Light';
+$mail->isHTML(true);                                  
+$mail->Subject = 'send from tzezotje';
 $mail->Body    = '
 		Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
